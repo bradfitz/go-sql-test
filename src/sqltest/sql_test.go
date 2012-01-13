4f43c192a0cdfa33b02bf9555cb23a2257765a11
Brad Fitzpatrick <brad@danga.com>
2012-01-12 17:34:37 -0800

    start of per-db test runners

--- src/sqltest/sql_test.go
+++ src/sqltest/sql_test.go
@@ -12,25 +12,32 @@ import (
 	_ "github.com/ziutek/mymysql/godrv"
 )
 
-func TestMisc(t *testing.T) {
+func testSQLite(t *testing.T, fn func(*testing.T, *sql.DB)) {
 	tempDir, err := ioutil.TempDir("", "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(tempDir)
-
-	var blob = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	db, err := sql.Open("sqlite3", filepath.Join(tempDir, "foo.db"))
 	if err != nil {
 		t.Fatalf("foo.db open fail: %v", err)
 	}
+	fn(t, db)
+}
+
+func TestBlobs_SQLite(t *testing.T) {
+	testSQLite(t, testBlobs)
+}
+
+func testBlobs(t *testing.T, db *sql.DB) {
+	var blob = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	db.Exec("create table foo (id integer primary key, bar blob[16])")
 	db.Exec("insert or replace into foo (id, bar) values(?,?)", 0, blob)
 
 	want := fmt.Sprintf("%x", blob)
 
 	b := make([]byte, 16)
-	err = db.QueryRow("select bar from foo where id = ?", 0).Scan(&b)
+	err := db.QueryRow("select bar from foo where id = ?", 0).Scan(&b)
 	got := fmt.Sprintf("%x", b)
 	if err != nil {
 		t.Errorf("[]byte scan: %v", err)
