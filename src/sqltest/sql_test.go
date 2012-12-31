accebcc45da734d871f92ea6c2edacc632942ded
Tamás Gulácsi <gt-dev@gthomas.eu>
2012-12-31 12:46:46 +0100

    Oracle needs :n as bind string, not ? or $n

--- src/sqltest/sql_test.go
+++ src/sqltest/sql_test.go
@@ -145,15 +145,21 @@ func (t params) mustExec(sql string, args ...interface{}) sql.Result {
 
 var qrx = regexp.MustCompile(`\?`)
 
-// q converts "?" characters to $1, $2, $n on postgres.
+// q converts "?" characters to $1, $2, $n on postgres, :1, :2, :n on Oracle
 func (t params) q(sql string) string {
-	if t.dbType != pq {
+	var pref string
+	switch t.dbType {
+	case pq:
+		pref = "$"
+	case oracle:
+		pref = ":"
+	default:
 		return sql
 	}
 	n := 0
 	return qrx.ReplaceAllStringFunc(sql, func(string) string {
 		n++
-		return "$" + strconv.Itoa(n)
+		return pref + strconv.Itoa(n)
 	})
 }
 
@@ -198,7 +204,8 @@ func (m *myMysqlDB) RunTest(t *testing.T, fn func(params)) {
 	}
 	for rows.Next() {
 		var table string
-		if rows.Scan(&table) == nil && strings.HasPrefix(table, TablePrefix) {
+		if rows.Scan(&table) == nil &&
+			strings.HasPrefix(strings.ToLower(table), strings.ToLower(TablePrefix)) {
 			params.mustExec("DROP TABLE " + table)
 		}
 	}
@@ -234,7 +241,8 @@ func (m *goMysqlDB) RunTest(t *testing.T, fn func(params)) {
 	}
 	for rows.Next() {
 		var table string
-		if rows.Scan(&table) == nil && strings.HasPrefix(table, TablePrefix) {
+		if rows.Scan(&table) == nil &&
+			strings.HasPrefix(strings.ToLower(table), strings.ToLower(TablePrefix)) {
 			params.mustExec("DROP TABLE " + table)
 		}
 	}
@@ -251,18 +259,26 @@ func (o *oracleDB) RunTest(t *testing.T, fn func(params)) {
 	if err != nil {
 		t.Fatalf("error connecting: %v", err)
 	}
+	defer db.Close()
 
 	params := params{oracle, t, db}
 
 	// Drop all tables in the test database.
-	rows, err := db.Query("SELECT table_name from user_tables WHERE table_name LIKE '" + TablePrefix + "%'")
+	rows, err := db.Query(`SELECT table_name FROM user_tables
+		WHERE UPPER(table_name) LIKE UPPER('` + TablePrefix + `%')`)
 	if err != nil {
 		t.Fatalf("failed to enumerate tables: %v", err)
 	}
+	var table sql.NullString
 	for rows.Next() {
-		var table string
-		if rows.Scan(&table) == nil {
-			params.mustExec("DROP TABLE " + table)
+		err = rows.Scan(&table)
+		t.Logf("Next => table=%+v err=%s", table, err)
+		if err != nil {
+			t.Fatalf("error reading table name: %s", err)
+		} else if !table.Valid {
+			t.Fatalf("error reading table name: null value!")
+		} else {
+			params.mustExec("DROP TABLE " + table.String)
 		}
 	}
 
@@ -270,11 +286,13 @@ func (o *oracleDB) RunTest(t *testing.T, fn func(params)) {
 }
 
 func sqlBlobParam(t params, size int) string {
-	if t.dbType == sqlite {
+	switch t.dbType {
+	case  sqlite:
 		return fmt.Sprintf("blob[%d]", size)
-	}
-	if t.dbType == pq {
+	case pq:
 		return "bytea"
+	case oracle:
+		return fmt.Sprintf("RAW(%d)", size)
 	}
 	return fmt.Sprintf("VARBINARY(%d)", size)
 }
@@ -345,9 +363,9 @@ func testTxQuery(t params) {
 	}
 	defer tx.Rollback()
 
-	_, err = tx.Exec("create table " + TablePrefix + "foo (id integer primary key, name varchar(50))")
+	_, err = t.DB.Exec("create table " + TablePrefix + "foo (id integer primary key, name varchar(50))")
 	if err != nil {
-		t.Fatal(err)
+		t.Logf("cannot drop table "+TablePrefix+"foo: %s", err)
 	}
 
 	_, err = tx.Exec(t.q("insert into "+TablePrefix+"foo (id, name) values(?,?)"), 1, "bob")
@@ -382,11 +400,11 @@ func TestPreparedStmt_PQ(t *testing.T)      { pq.RunTest(t, testPreparedStmt) }
 func TestPreparedStmt_Oracle(t *testing.T)  { oracle.RunTest(t, testPreparedStmt) }
 
 func testPreparedStmt(t params) {
-	t.mustExec("CREATE TABLE " + TablePrefix + "t (count INT)")
 	sel, err := t.Prepare("SELECT count FROM " + TablePrefix + "t ORDER BY count DESC")
 	if err != nil {
 		t.Fatalf("prepare 1: %v", err)
 	}
+	t.mustExec("CREATE TABLE " + TablePrefix + "t (count INT)")
 	ins, err := t.Prepare(t.q("INSERT INTO " + TablePrefix + "t (count) VALUES (?)"))
 	if err != nil {
 		t.Fatalf("prepare 2: %v", err)
